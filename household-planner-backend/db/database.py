4b3e2ef9681ab27da1e4c0abb5d110c3806ab117
matikoss <m.kossakowski0@gmail.com>
2021-12-04 17:25:09 +0100

    MainService initalization

---
+++ household-planner-backend/db/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from db.database_settings import db_settings
+
+SQLALCHEMY_DATABASE_URL = db_settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
