c8cadd747dc5b9beda26a00221d6dc3ebe80fd73
matikoss <m.kossakowski0@gmail.com>
2021-12-05 19:12:34 +0100

    Some db stuff

--- household-planner-backend/db/database.py
+++ household-planner-backend/db/database.py
@@ -1,11 +1,32 @@
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from db.database_settings import db_settings
+from db.database_settings import prod
 
-SQLALCHEMY_DATABASE_URL = db_settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def create_connection_engine():
+    if prod:
+        return create_engine(
+            sqlalchemy.engine.url.URL.create(
+                drivername="postgresql+pg8000",
+                username=db_settings.POSTGRES_USER,
+                password=db_settings.POSTGRES_PASSWORD,
+                database=db_settings.POSTGRES_DB,
+                query={
+                    "unix_sock": "{}/{}/.s.PGSQL.5432".format(
+                        "/cloudsql",
+                        db_settings.POSTGRES_SERVER)
+                }
+            )
+        )
+    SQLALCHEMY_DATABASE_URL = db_settings.DATABASE_URL
+    return create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+engine = create_connection_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
