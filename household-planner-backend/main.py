a84d07b0e3d86baf781206eab4d913075a66e9a7
ErnestSzypula <ernest.szypula@ochk.pl>
2022-01-04 22:11:42 +0100

    firebase auth 1 try

--- household-planner-backend/main.py
+++ household-planner-backend/main.py
@@ -6,6 +6,13 @@ from db.database import engine
 from models import chore, household, household_members, user
 from routers import households, users, members
 
+import firebase_admin
+
+from fastapi import Depends
+from auth.auth import get_firebase_token
+
+firebase_admin.initialize_app()
+
 chore.Base.metadata.create_all(bind=engine)
 household.Base.metadata.create_all(bind=engine)
 household_members.Base.metadata.create_all(bind=engine)
@@ -31,3 +38,8 @@ app.include_router(members.router, prefix="/api")
 @app.get("/api")
 async def root():
     return {"message": "test_deploy"}
+
+
+@app.get("/user/", tags=["user"])
+async def hello_user(firebase_token = Depends(get_firebase_token)):
+    return firebase_token
