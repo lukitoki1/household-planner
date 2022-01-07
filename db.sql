ae76797eb230d716c0679e387caed2bac052c810
Łukasz Kamiński <lukasz.kaminski@ochk.pl>
2022-01-07 22:34:59 +0100

    Updated users table: dropped phone number

--- db.sql
+++ db.sql
@@ -1,2 +1,3 @@
 ALTER TABLE users ADD CONSTRAINT ix_user_name_unique UNIQUE (user_email);
 ALTER TABLE users ADD COLUMN user_notification_email TEXT;
+ALTER TABLE users DROP COLUMN user_phone_number;
