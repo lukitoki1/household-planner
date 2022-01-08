e1a7f139b5161ca4b38f3b4d44ff835598acf9ae
pachniar <01130999@pw.edu.pl>
2022-01-08 02:09:19 +0100

    Translation works

--- translation-service/translation.py
+++ translation-service/translation.py
@@ -1,25 +1,23 @@
-
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="gtranslate-key.json"
 
 def get_translation(lang: str, text: str):
-    dict = {}
-    dict['lang']=lang
-    dict['text']=text
     import six
     from google.cloud import translate_v2 as translate
-
     translate_client = translate.Client()
-
     if isinstance(text, six.binary_type):
         text = text.decode("utf-8")
 
     # Text can also be a sequence of strings, in which case this method
     # will return a sequence of results for each text.
-    result = translate_client.translate(text, target_language=target)
+    result = translate_client.translate(text, target_language=lang)
 
     print(u"Text: {}".format(result["input"]))
     print(u"Translation: {}".format(result["translatedText"]))
     print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
-
+    dict={}
+    dict['lang']=lang
+    dict['text']=result['translatedText']
     return dict
 
 
